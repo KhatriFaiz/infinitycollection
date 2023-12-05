0b7afdc136d78f0a23c1a7e54cdb3e81c3441e73
Faiz Khatri <faizkhatri00@gmail.com>
2023-12-05 12:16:58 +0530

    Added a loading state for the products overveiw page.

---
+++ app/(app)/product/[slug]/loading.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+
+const Loading = () => {
+  return (
+    <Container sx={{ paddingBlock: 5, textAlign: "center" }}>
+      <div>Loading Product ...</div>
+    </Container>
+  );
+};
+
+export default Loading;
